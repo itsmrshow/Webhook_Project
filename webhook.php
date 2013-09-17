f0b140f658763da91f2c60a913b1111624cd58d2
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-17 15:02:25 -0400

    testing payload

--- webhook.php
+++ webhook.php
@@ -7,7 +7,7 @@ function stripslashes_deep($value){
                 stripslashes($value);
     return $value;
 }
-$result = stripslashes_deep($_REQUEST['rawRequest']);
+$result = stripslashes_deep($_REQUEST['payload']);
 
 //email data
 $emailfrom = "robert.seccareccia.jr@gmail.com"; //Sender, replace with your email
@@ -19,19 +19,20 @@ $obj = json_decode($result, true);
 // prepare email body text
 $body = "Contact Form Submissions"; //Title
 $body .= "\n";  //Nothing but new line
-$body .= "Name: ". $obj['name']; //Print Name
+$body .= "Name: ". $obj['after']; //Print Name
 $body .= "\n";
-$body .= "Config: ". $obj['config']; //Print Email
+$body .= "Config: ". $obj['before']; //Print Email
 $body .= "\n";
-$body .= "Events: ". $obj['events']; //Print Message
+$body .= "Events: ". $obj['ref']; //Print Message
 $body .= "\n";
-$body .= "Active: ". $obj['active']; //Print Message
+$body .= "Active: ". $obj['commits']; //Print Message
 $body .= "\n";
-$body .= "URL: ". $obj['url']; //Print Message
+$body .= "URL: ". $obj['id']; //Print Message
 $body .= "\n";
-$body .= "Content type: ". $obj['content_type']; //Print Message
+$body .= "Content type: ". $obj['message']; //Print Message
 $body .= "\n";
-$body .= "Secret: ". $obj['secret']; //Print Message
+$body .= "Secret: ". $obj['url']; //Print Message
+
 
 // send email
 mail($emailto, $subject, $body, "From: <$emailfrom>");
