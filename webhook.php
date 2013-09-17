041e8c5ba72a9a3ca14b740757044bbb592d46b6
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-17 11:18:18 -0400

    testing
    testing to see if this works

--- webhook.php
+++ webhook.php
@@ -7,22 +7,11 @@
         {
                 // Decode the payload json string
                 $payload = json_decode($_REQUEST['payload']);
+				echo $payload;
         }
         catch(Exception $e)
         {
                 exit;
         }
  
-        // Pushed to master?
-        if ($payload->ref === 'refs/heads/master')
-        {
-            // Log the payload object
-            @file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
- 
-            // Prep the URL - replace https protocol with git protocol to prevent 'update-server-info' errors
-            $url = str_replace('https://', 'git://', $payload->repository->url);
- 
-            // Run the build script as a background process
-            `./build.sh {$url} {$payload->repository->name} > /dev/null 2>&1 &`;
-        }
 ?>
