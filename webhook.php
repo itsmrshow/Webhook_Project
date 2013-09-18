15bf07a2e39c065153749b11fcf5ac8c484c94cf
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-18 13:37:01 -0400

    created an xml generator

--- webhook.php
+++ webhook.php
@@ -16,6 +16,37 @@ $subject = "Github Test Webhook"; //Email Subject
 
 $obj = json_decode($result, true);
 
+$student_info = array(your array data);
+
+// creating object of SimpleXMLElement
+$xml_git = new SimpleXMLElement("<?xml version=\"1.0\"?><object></object>");
+
+// function call to convert array to xml
+array_to_xml($obj,$xml_git);
+
+//saving generated xml file
+//$xml_git->asXML('/xml');
+
+
+// function defination to convert array to xml
+function array_to_xml($obj, &$xml_git) {
+    foreach($obj as $key => $value) {
+        if(is_array($value)) {
+            if(!is_numeric($key)){
+                $subnode = $xml_git->addChild("$key");
+                array_to_xml($value, $subnode);
+            }
+            else{
+                $subnode = $xml_git->addChild("item$key");
+                array_to_xml($value, $subnode);
+            }
+        }
+        else {
+            $xml_git->addChild("$key","$value");
+        }
+    }
+
+
 // prepare email body text
 $body = "Contact Form Submissions"; //Title
 $body .= "\n";  //Nothing but new line
@@ -33,15 +64,24 @@ $body .= "timestamp: ". ($obj['commits'][0]['timestamp']); //Print Message
 $body .= "\n";
 $body .= "URL: ". ($obj['commits'][0]['url']); //Print Message
 $body .= "\n";
-$body .= "Added: ". ($obj['commits'][0]['added']); //Print Message
-$body .= "\n";
-$body .= "Removed: ". ($obj['commits'][0]['removed']); //Print Message
-$body .= "\n";
-$body .= "modified: ". ($obj['commits'][0]['modified']); //Print Message
-$body .= "\n";
+
 
 // send email
 mail($emailto, $subject, $body, "From: <$emailfrom>");
+
+$url = "http://seccareccia.crisply.com/api/"
+$xml_git;
+
+$post_data = array('xml' => $xml_git);
+$stream_options = array(
+    'http' => array(
+        'method'  => 'POST',
+        'header'  => 'Content-type: application/xml; charset=UTF-8' . "\r\n",
+        'content' =>  http_build_query($post_data)));
+
+$context  = stream_context_create($stream_options);
+$response = file_get_contents($url, null, $context);
+
 ?>
 <html>
 <head>
