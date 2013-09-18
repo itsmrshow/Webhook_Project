82b5bdce89e8219f7df3c9fb2546c0e1113cfff7
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-18 17:00:05 -0400

    trying to save part 2

--- webhook.php
+++ webhook.php
@@ -49,9 +49,9 @@ $title = $root->appendChild($title);
 $text = $doc->createTextNode($obj['commits'][0]['timestamp']);
 $text = $title->appendChild($text);
 
-$body .= $doc->saveXML(/xml/activity-item.xml) . "\n";
+$body .= $doc->saveXML() . "\n";
 
-//$doc -> save('activity-item');
+$doc->save("/xml/activity-item.xm");
 
 //add comment
 // send email
