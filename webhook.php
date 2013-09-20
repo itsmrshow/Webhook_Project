9a6739534aa4c0cc8d5c73606c294f7d6e8f949f
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-19 20:51:23 -0400

    added xml

--- webhook.php
+++ webhook.php
@@ -70,7 +70,7 @@ curl_setopt($ch, CURLOPT_MUTE, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml_data");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $doc->saveXML());
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 curl_close($ch);
