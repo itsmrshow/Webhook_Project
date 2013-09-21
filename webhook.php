547ea06bf1f1f2c4b62165f7e42b3578a0448cc1
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-21 14:05:04 -0400

    trying more curl

--- webhook.php
+++ webhook.php
@@ -36,35 +36,59 @@ $body .= "\n";
 $body .= "URL: ". ($obj['commits'][0]['url']); //Print Message
 $body .= "\n";
 
-$doc  = '<?xml version="1.0" encoding="utf-8"?><activity-item xmlns="http://crisply.com/api/v1"><guid>github-activity-'.($obj['commits'][0]['timestamp']).'</guid><text>'.($obj['commits'][0]['message']).'</text></activity-item>'
+$doc  = new DOMDocument('1.0', 'utf-8');
 
-//$URL = "http://seccareccia.crisply.com/api/activity_items.xml";
-//$URL = "http://requestb.in/135vbk21";
-$user = "m1Qy3WWSV1IbISTe4EBD";
-$pass = "";
-$host = "http://requestb.in";
-$path = "/135vbk21";
-$agent = "Mozilla/4.0 
-(compatible; MSIE 5.5; Windows NT 5.0)";
+$doc->formatOutput = true;
 
-$fp = fsockopen($host,80, &$err_num, &$err_msg, 10);
-if (!$fp) {
-echo "$err_msg ($err_num)<br>\n";
-} else {
-$auth = $user.":".$pass ; 
-$string=base64_encode($auth);
-echo $string;
-fputs($fp, "POST $path HTTP/1.1\r\n");
-fputs($fp, "Authorization: Basic ".$string."\r\n");
-fputs($fp, "User-Agent: ".$agent."\n");
-fputs($fp, "Host: $host\n");
-fputs($fp, "Content-type: application/x-www-form-urlencoded\n");
-fputs($fp, "Content-length: ".strlen($doc)."\n");
-fputs($fp, "Connection: close\n\n");
-fputs($fp, $doc);
-for ($i = 1; $i < 10; $i++){$reply = fgets($fp, 256);}
-fclose($fp);
-}
+$root = $doc->createElementNS('http://crisply.com/api/v1', 'activity-item');
+$doc->appendChild($root);
+
+$title = $doc->createElement('guid');
+$title = $root->appendChild($title);
+
+$text = $doc->createTextNode('github-activity-' .($obj['commits'][0]['timestamp']));
+$text = $title->appendChild($text);
+
+$title = $doc->createElement('text');
+$title = $root->appendChild($title);
+
+$text = $doc->createTextNode($obj['commits'][0]['message']);
+$text = $title->appendChild($text);
+
+$title = $doc->createElement('author');
+$title = $root->appendChild($title);
+
+$text = $doc->createTextNode($obj['commits'][0]['author']['email']);
+$text = $title->appendChild($text);
+
+$title = $doc->createElement('duration');
+$title = $root->appendChild($title);
+
+$text = $doc->createTextNode(20);
+$text = $title->appendChild($text);
+
+$body .= $doc->saveXML() . "\n";
+
+
+$username = "m1Qy3WWSV1IbISTe4EBD";
+$password = "";
+
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml', 'charset: UTF-8'));
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_URL, 'seccareccia.crisply.com/api/activity_items.xml'); 
+curl_setopt($ch, CURLOPT_USERPWD, $username.':'.$password);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_TIMEOUT, 4); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $doc->saveXML()); 
+ 
+$result = curl_exec($ch); 
+ 
+//Close the handle
+curl_close($ch);
+ 
 
 mail($emailto, $subject, $body, "From: <$emailfrom>");
 ?>
