595c148ef1653b3cbc80b67a963aaf550754899a
Robert Seccareccia <seccareccir1@owls.southernct.edu>
2013-09-21 14:59:13 -0400

    trying new curl class

--- webhook.php
+++ webhook.php
@@ -56,31 +56,75 @@ $text = $title->appendChild($text);
 
 $body .= $doc->saveXML() . "\n";
 
-$tuCurl = curl_init(); 
-curl_setopt($tuCurl, CURLOPT_URL, "http://requestb.in/13w5d631"); 
-curl_setopt($tuCurl, CURLOPT_PORT , 443); 
-curl_setopt($tuCurl, CURLOPT_VERBOSE, 0); 
-curl_setopt($tuCurl, CURLOPT_HEADER, 0); 
-curl_setopt($tuCurl, CURLOPT_SSLVERSION, 3); 
-curl_setopt($tuCurl, CURLOPT_SSLCERT, getcwd() . "/client.pem"); 
-curl_setopt($tuCurl, CURLOPT_SSLKEY, getcwd() . "/keyout.pem"); 
-curl_setopt($tuCurl, CURLOPT_CAINFO, getcwd() . "/ca.pem"); 
-curl_setopt($tuCurl, CURLOPT_POST, 1); 
-curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, 1); 
-curl_setopt($tuCurl, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($tuCurl, CURLOPT_POSTFIELDS, $doc->saveXML()); 
-curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/xml","charset: utf-8", "X-Crisply-Authentication: m1Qy3WWSV1IbISTe4EBD")); 
+//curl_setopt($tuCurl, CURLOPT_HTTPHEADER, array("Content-Type: application/xml","charset: utf-8", "X-Crisply-Authentication: m1Qy3WWSV1IbISTe4EBD")); 
 
-$tuData = curl_exec($tuCurl); 
-if(!curl_errno($tuCurl)){ 
-  $info = curl_getinfo($tuCurl); 
-  echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url']; 
-} else { 
-  echo 'Curl error: ' . curl_error($tuCurl); 
+class cURL { 
+var $headers; 
+var $user_agent; 
+var $compression; 
+var $cookie_file; 
+var $proxy; 
+function cURL($cookies=TRUE,$cookie='cookies.txt',$compression='gzip',$proxy='') { 
+$this->headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg'; 
+$this->headers[] = 'Connection: Keep-Alive'; 
+$this->headers[] = 'Content-type: application/xml;charset=UTF-8'; 
+$this->user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)'; 
+$this->compression=$compression; 
+$this->proxy=$proxy; 
+$this->cookies=$cookies; 
+if ($this->cookies == TRUE) $this->cookie($cookie); 
 } 
-
-curl_close($tuCurl); 
-echo $tuData; 
+function cookie($cookie_file) { 
+if (file_exists($cookie_file)) { 
+$this->cookie_file=$cookie_file; 
+} else { 
+fopen($cookie_file,'w') or $this->error('The cookie file could not be opened. Make sure this directory has the correct permissions'); 
+$this->cookie_file=$cookie_file; 
+fclose($this->cookie_file); 
+} 
+} 
+function get($url) { 
+$process = curl_init($url); 
+curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers); 
+curl_setopt($process, CURLOPT_HEADER, 0); 
+curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent); 
+if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file); 
+if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file); 
+curl_setopt($process,CURLOPT_ENCODING , $this->compression); 
+curl_setopt($process, CURLOPT_TIMEOUT, 30); 
+if ($this->proxy) curl_setopt($process, CURLOPT_PROXY, $this->proxy); 
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1); 
+$return = curl_exec($process); 
+curl_close($process); 
+return $return; 
+} 
+function post($url,$data) { 
+$process = curl_init($url); 
+curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers); 
+curl_setopt($process, CURLOPT_HEADER, 1); 
+curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent); 
+if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file); 
+if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file); 
+curl_setopt($process, CURLOPT_ENCODING , $this->compression); 
+curl_setopt($process, CURLOPT_TIMEOUT, 30); 
+if ($this->proxy) curl_setopt($process, CURLOPT_PROXY, $this->proxy); 
+curl_setopt($process, CURLOPT_POSTFIELDS, $data); 
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1); 
+curl_setopt($process, CURLOPT_POST, 1); 
+$return = curl_exec($process); 
+curl_close($process); 
+return $return; 
+} 
+function error($error) { 
+echo "<center><div style='width:500px;border: 3px solid #FFEEFF; padding: 3px; background-color: #FFDDFF;font-family: verdana; font-size: 10px'><b>cURL Error</b><br>$error</div></center>"; 
+die; 
+} 
+} 
+$cc = new cURL(); 
+//$cc->get('http://requestb.in/13w5d631'); 
+$cc->post('http://requestb.in/13w5d631',$doc->saveXML()); 
 
 mail($emailto, $subject, $body, "From: <$emailfrom>");
 ?>
